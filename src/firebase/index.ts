c65b637d05516b4f157e6bf180b1b4571373462f
llvlldat <minhdat.dongoc@gmail.com>
2024-03-12 23:02:10 +0700

    change database

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,13 +6,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: 'AIzaSyAyJSHiqBkPnQ3Rh25dUb0lvI0UIzZ-m9Y',
-    authDomain: 'wedding-wishes-2c893.firebaseapp.com',
-    projectId: 'wedding-wishes-2c893',
-    storageBucket: 'wedding-wishes-2c893.appspot.com',
-    messagingSenderId: '615307480898',
-    appId: '1:615307480898:web:a89450bdbe3d74ed4602db',
-    measurementId: 'G-N98MR3JJLM',
+    apiKey: 'AIzaSyBYGDv9lj8gk_F1scEXgWKKwiMcacrnCzU',
+    authDomain: 'dathue-wedding.firebaseapp.com',
+    projectId: 'dathue-wedding',
+    storageBucket: 'dathue-wedding.appspot.com',
+    messagingSenderId: '192974160270',
+    appId: '1:192974160270:web:18f0ae71ad5fac3ae724df',
+    measurementId: 'G-7D1V2PXF3B',
 }
 
 // Initialize Firebase
